f847a0bc86bd43aebb4455c065b911ba6bf87de0
whereishikaru <h.suzuki.028@gmail.com>
2024-10-25 09:54:09 +1300

    initial commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,30 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+import { FC } from 'react';
+
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//     return (
+//         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+//             <div className="w-full flex-none md:w-64">
+//                 <SideNav />
+//             </div>
+//             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+//         </div>
+//     );
+// }
+
+type LayoutProps = {
+    children: React.ReactNode;
+};
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+                <SideNav />
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+    );
+}
+
+export default Layout;
